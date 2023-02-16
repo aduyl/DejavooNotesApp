ea186d95a1dcfb736cc1c2bda35ab07b76fbacc8
adyul <luidasukh@gmail.com>
2023-02-16 13:34:19 +0300

    Commit

---
+++ DejavooNotesApp/Modules/Test/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  DejavooNotesApp
+//
+//  Created by mac on 16.02.2023.
+//
+
+import Foundation
+
+final class Test {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
